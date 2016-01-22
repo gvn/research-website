bcddd894b9e66e73d6f528bf915b5c6f164332a5
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-01-22 13:52:25 -0800

    adding local storage persistence

--- source/cms/js/cms.js
+++ source/cms/js/cms.js
@@ -1,39 +1,68 @@
+JSONEditor.defaults.theme = 'bootstrap3';
+JSONEditor.defaults.iconlib = 'bootstrap3';
+
 // Initialize the editor with a JSON schema
-var editor = new JSONEditor(document.getElementById('editor_holder'),{
+var editor = new JSONEditor(document.getElementById('editor'),{
+  // TODO : Make schema external
   schema: {
-    type: "object",
-    title: "Car",
-    properties: {
-      make: {
-        type: "string",
-        enum: [
-          "Toyota",
-          "BMW",
-          "Honda",
-          "Ford",
-          "Chevy",
-          "VW"
-        ]
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "id": "mofoResearch",
+    "type": "object",
+    "properties": {
+      "headline": {
+        "id": "mofoResearch/headline",
+        "type": "string"
       },
-      model: {
-        type: "string"
+      "subhead": {
+        "id": "mofoResearch/subhead",
+        "type": "string"
       },
-      year: {
-        type: "integer",
-        enum: [
-          1995,1996,1997,1998,1999,
-          2000,2001,2002,2003,2004,
-          2005,2006,2007,2008,2009,
-          2010,2011,2012,2013,2014
-        ],
-        default: 2008
+      "subhead2": {
+        "id": "mofoResearch/subhead2",
+        "type": "string"
+      },
+      "blockPairs": {
+        "id": "mofoResearch/blockPairs",
+        "type": "array",
+        "items": {
+          "id": "mofoResearch/blockPairs/0",
+          "type": "object",
+          "properties": {
+            "location": {
+              "id": "mofoResearch/blockPairs/0/location",
+              "type": "string"
+            },
+            "date": {
+              "id": "mofoResearch/blockPairs/0/date",
+              "type": "string"
+            },
+            "reportURL": {
+              "id": "mofoResearch/blockPairs/0/reportURL",
+              "type": "string"
+            },
+            "description": {
+              "id": "mofoResearch/blockPairs/0/description",
+              "type": "string"
+            }
+          }
+        }
       }
-    }
+    },
+    "required": [
+      "headline",
+      "subhead",
+      "subhead2",
+      "blockPairs"
+    ]
   }
 });
 
-// Hook up the submit button to log to the console
+editor.setValue(JSON.parse(localStorage.getItem('page')));
+
 document.getElementById('submit').addEventListener('click',function() {
-  // Get the value from the editor
-  console.log(editor.getValue());
+  var pageJSON = editor.getValue();
+
+  console.log(pageJSON);
+
+  localStorage.setItem('page', JSON.stringify(pageJSON));
 });
